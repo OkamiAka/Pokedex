766ab0183221f2500212582bcfc1a385e5b38f5a
OkamiAka <nils.caqueux@gmail.com>
2023-03-27 21:09:47 +0200

    react basics 04

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,9 @@
-            const pokemonList = [
-                {
-                  name: "bulbasaur",
-                  imgSrc:
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-                },
-                {
-                  name: "mew",
-                },
-              ];
-function PokemonCard() {
-    const pokemon = pokemonList[0]
+function PokemonCard(props) {
+    const pokemon = props
     return (
         <div>
             <figure>
-                {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} />: <p>???</p>}
+                {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
                 <figcaption>{pokemon.name}</figcaption>
             </figure>
         </div>
