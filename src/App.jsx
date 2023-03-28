838217cba1f7ed5677703b435cfb1c92ddf55c36
OkamiAka <nils.caqueux@gmail.com>
2023-03-28 22:59:58 +0200

    color card

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,44 @@ import { useState } from "react";
 import './App.css'
 
 const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    color: "lightgreen",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    color: "lightsalmon",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    color: "lightblue",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    color: "yellow",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
-  
+
 
   const [count, setCount] = useState(0);
 
-  
+
 
   return (
     <div>
-      <NavBar setCount={setCount}/>
+      <NavBar setCount={setCount} />
       <PokemonCard pokemon={pokemonList[count]} />
     </div>
   )
@@ -44,9 +48,9 @@ function App() {
 
 function NavBar(props) {
   return (<>
-      {pokemonList.map((pokemon, index) => (
-        <button onClick={() => { props.setCount(index) }} key={pokemon.name}>{pokemon.name}</button>
-      ))}
+    {pokemonList.map((pokemon, index) => (
+      <button onClick={() => { props.setCount(index) }} key={pokemon.name}>{pokemon.name}</button>
+    ))}
   </>);
 
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 function PokemonCard({pokemon}) {
     return (
-            <figure className='card'>
+            <figure className='card' style={{backgroundColor: pokemon.color}}>
                 {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} className='card-img'/> : <p>???</p>}
                 <figcaption>{pokemon.name}</figcaption>
             </figure>
