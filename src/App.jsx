7221a922c1525061c6dfdb667e547bca2f1770a0
OkamiAka <nils.caqueux@gmail.com>
2023-03-28 11:55:16 +0200

    react basics 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
 import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const [count, setCount] = useState(0);
+
+  const suivant = () => {
+    if (count < pokemonList.length-1) { setCount(count + 1) }
+  }
+  const precedente = () => {
+    if (count > 0) { setCount(count - 1) }
+
+  }
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[count]} />
+      <button onClick={precedente}>précédente</button>
+      <button onClick={suivant}>suivant</button>
+      <br/><p>{count+1}/{pokemonList.length}</p>
     </div>
   )
 }
-const pokemonList = [
-  {
-      name: "bulbasaur",
-      imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-      name: "mew",
-  },
-];
+
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
 function PokemonCard({pokemon}) {
     return (
-        <div>
             <figure>
                 {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
                 <figcaption>{pokemon.name}</figcaption>
             </figure>
-        </div>
     )
 }
 
